407d9c2b24d4db349fe855a8485fd6c23d86fb74
Diego Vilela <diegovilelask8@gmail.com>
2025-04-20 17:05:26 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
-console.log("Hello, world!");
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+      once: true // anima apenas uma vez
+    });
+  
+    const contador = document.getElementById('contador');
+    if (!contador) return;
+  
+    const dataDoEvento = new Date('April 26 2025 09:00:00 GMT-0300');
+    const timeStampDoEvento = dataDoEvento.getTime();
+  
+    const contaAsHoras = setInterval(function () {
+      const agora = new Date();
+      const timeStampAtual = agora.getTime();
+      const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+  
+      const diasEmMs = 1000 * 60 * 60 * 24;
+      const horasEmMs = 1000 * 60 * 60;
+      const minutoEmMs = 1000 * 60;
+      const segundosEmMs = 1000;
+  
+      const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+      const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+      const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutoEmMs);
+      const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / segundosEmMs);
+  
+      contador.innerHTML = `${diasAteOEvento} d ${horasAteOEvento} h ${minutosAteOEvento} m ${segundosAteOEvento} s`;
+  
+      if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        contador.innerHTML = 'O evento já aconteceu!';
+      }
+    }, 1000);
+  });
